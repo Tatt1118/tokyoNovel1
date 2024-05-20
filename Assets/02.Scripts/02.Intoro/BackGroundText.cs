5013d036dbd44c517eedee0c3db3b812d6abc91b
tacagi003 <92666044+Tatt1118@users.noreply.github.com>
2024-05-20 18:45:29 +0900

    second

--- Assets/02.Scripts/02.Intoro/BackGroundText.cs
+++ Assets/02.Scripts/02.Intoro/BackGroundText.cs
@@ -1,68 +1,49 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
-using UnityEngine.SceneManagement;
-using Cysharp.Threading.Tasks;
 
 public class BackGroundText : MonoBehaviour
 {
     [SerializeField] private IntroNovel text;
     [SerializeField] private Text telopText;
     private float telopSpeed = 0.1f;
-    private float nextTextTm = 2.3f;
-    private int originalFontSize = 52;
     private int dialogueListIndex = 0;
-    public Image backgroundImage;
-    public GameObject backgroundImage2;
-    [SerializeField] public MainStorycs mainStorycs;
+    [SerializeField] private MainStorycs mainStorycs;
 
-    async void Start()
+    private void Start()
     {
-        await StartTelop();
+        TextDisplay();
     }
 
-    private async UniTask StartTelop()
+    public void Update()
     {
-
-        while (dialogueListIndex < text.novel.Count)
-        {
-            string currentDialogue = text.novel[dialogueListIndex];
-            await DisplayText(currentDialogue);
-            await UniTask.Delay((int)(nextTextTm * 1000));
-            dialogueListIndex++;
-
-            // if (dialogueListIndex == 1) // 13番目の場合
-            // {
-            //     OnAllTextDisplayed();
-            // }
-
-        }
-
-        // 背景画像のフェードアウトとテキストのフェードインを同時に実行する
-        var sequence = DOTween.Sequence();
-        var sequenceCompletionSource = new UniTaskCompletionSource();
-
-        sequence.Append(backgroundImage.DOFade(0.0f, 3f));
-        sequence.Join(telopText.DOFade(0f, 3f));
-        sequence.OnComplete(() => sequenceCompletionSource.TrySetResult());
-
-        // 両方のTweenが完了するまで待機する
-        await sequenceCompletionSource.Task;
-
-        // フェードアウトとフェードインが完了した後に実行する処理
-        OnAllTextDisplayed();
-    }
-
-    private async UniTask DisplayText(string text)
-    {
-        float displayTime = text.Length * telopSpeed;
-        telopText.text = ""; // テキストのリセット
-        await telopText.DOText(text, displayTime).SetEase(Ease.Linear).AsyncWaitForCompletion(); // 非同期で文字表示
+        if (Input.GetMouseButtonDown(0)) TextDisplay();
     }
 
     private void OnAllTextDisplayed()
     {
         mainStorycs.NaninovelInitial();
         Debug.Log("すべてのテキストが表示されました。");
+    }
+
+    public void TextDisplay()
+    {
+        if (DOTween.IsTweening(telopText)) //IsTweening（）便利　dotweenが動いてるか判定してくれる
+        {
+            telopText.text = text.novel[dialogueListIndex - 1];
+            telopText.DOKill();
+        }
+        else
+        {
+            if (dialogueListIndex >= text.novel.Count)
+            {
+                OnAllTextDisplayed();
+                return;
+            }
+            string currentDialogue = text.novel[dialogueListIndex];
+            telopText.text = "";
+            telopText.DOText(text.novel[dialogueListIndex], text.novel[dialogueListIndex].Length * telopSpeed).SetEase(Ease.Linear);
+            dialogueListIndex++;
+        }
     }
 }
